97e35cb6020b2ad56134142661d5752f603a02f9
Lythi45 <scala.bine@gmail.com>
2018-12-03 16:59:17 +0100

    day3 second part

--- day3.py
+++ day3.py
@@ -8,3 +8,20 @@ for b in bl:
       for y in range(sy):
           f[(px+x,py+y)]=f.get((px+x,py+y),0)+1
 print(sum([1 for x in f if f[x]>1 ]))
+
+for b in bl:
+  s=b.split(':')
+  id = int(b.split('@')[0][1:])
+  px,py=map(int,s[0].split('@')[1].split(','))
+  sx,sy=map(int,s[1].split('x'))
+  for x in range(sx):
+    br=False
+    for y in range(sy):
+      if f[(px+x,py+y)]>1:
+        br=True
+        break
+    if br:
+        break
+  else:
+      print(id)
+      break
