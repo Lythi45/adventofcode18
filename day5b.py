bc25bb1d2c4483b1c66b2698994dc25329c1f173
Lythi45 <scala.bine@gmail.com>
2018-12-05 15:31:38 +0100

    trying to improve time

--- day5b.py
+++ day5b.py
@@ -3,15 +3,12 @@ st_ti=time.process_time()
 
 poly=open("day5.txt","r").read().strip()
 
-def l_react(str):
-react=True
-while react:
-    react=False
-    np=""
-    i=0
-    while i < len(poly)-1:
-        a=poly[i]
-        b=poly[i+1]
+def l_react(poly):
+    i=1
+    j=2
+    a=poly[i]
+    b=poly[j]
+    while j < len(poly)-1:
         if a.upper()==b.upper() and a!=b:
             react=True
             i+=2
